92543d47a94afd546617e2c87b090af35024158d
Bruno Sampaio <dev.brunno9@gmail.com>
2024-03-19 19:47:49 -0400

    Começando o projeto aqui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-amber-600">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className=" text-9xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
